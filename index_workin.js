7d5616bf88dabcd7e0f28c3b47e12f3dc29d70f8
Unknown <mou.wang@104.com.tw>
2018-04-15 21:17:39 +0800

    flip back first.

---
+++ index_workin.js
@@ -0,0 +1,43 @@
+var request = require('request');
+
+//Extract Lists - add more data here if needed.
+var LeoArray =  require('data/_LeoArray.json');
+var FuckArray = require('data/_FuckArray.json');
+var WorkArray = require('data/_WorkArray.json');
+var chokeArray = require('data/_chokeArray.json');
+var ssdArray = require('data/_ssdArray.json');
+var ceoArray = require('data/_ceoArray.json');
+var hentaiArray = require('data/_hentaiArray.json');
+
+exports.handler = function (req, res, callback) {
+
+    console.log('TestData0415-911pm');
+    console.log(JSON.stringify(req));
+
+    const ChannelAccessToken = process.env['CHANNEL_ACCESS_TOKEN'];
+    var _HeaderSetting = {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": " Bearer " + ChannelAccessToken
+    };
+
+    var messages = 'Now Testing...Fuck you, Don?';
+    var reply_token = req.events.replyToken;
+    var options = {
+        method: 'POST',
+        uri: "https://api.line.me/v2/bot/message/reply",
+        headers: _HeaderSetting,
+        json: true,
+        body: {
+            replyToken: reply_token,
+            messages
+        }
+    };
+
+    request.post(options, function (err, response, body) {
+
+        console.log('success');
+        callback(null, 'AND?');
+    });
+
+    callback(null, 'What the?');
+};
