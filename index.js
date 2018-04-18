c0ad5742cd4e43ec1c009545898d09044272a4da
Mou-Chun Wang <mou.wang@g.ncu.edu.tw>
2018-04-18 20:03:05 +0800

    try to use Base64 Images

--- index.js
+++ index.js
@@ -60,9 +60,13 @@ exports.handler = function (req, res) {
                 console.log(body);
                 imageName = img_id + '.jpg';
 
+                var prefix = "data:image/png;base64, ";
+                var base64Image = new Buffer(body, 'binary').toString('base64');
+                base64Image = prefix + base64Image;
+
                 //Here Upload to S3
                 var s3Bucket = new AWS.S3({params:{Bucket:PEPEBOT_S3_BUCKET} });
-                var data = {Key: imageName, Body: body};
+                var data = {Key: imageName, Body: base64Image};
                 s3Bucket.upload(data, function(err, data){
                     if (err)
                     { console.log('Error uploading data: ', data);}
