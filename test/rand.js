6244f44c87501a997850c6f930417a932200750c
Mou <mou.wang@g.ncu.edu.tw>
2019-05-13 09:41:03 +0800

    Update and integrate Mocha w/Travis

--- test/rand.js
+++ test/rand.js
@@ -5,14 +5,14 @@ describe('#libs/rand', () => {
     // 從陣列隨機抽出 1 個值
     it('從陣列隨機抽出 1 個值', done => {
         const v = rand.getFromArray([1, 2, 3, 4]);
-        console.log(v);
+        // console.log(v);
         v.should.Number();
         done();
     });
 
     it('隨機出 1 個不超過最大值的值', done => {
         const v = rand.getValue(999);
-        console.log(v);
+        // console.log(v);
         v.should.Array();
         done();
     });
