19755ab5c3107a3b72698e05e7e5419e5f2b3830
Mou <mou@zuvio.com>
2019-05-21 09:45:00 +0800

    Add basics

--- index.js
+++ index.js
@@ -496,6 +496,10 @@ exports.handler = function (req, res) {
                 var limit = 9999;
                 var intimacy = rand.getNumber(0, limit);
 
+                if(speaker == 'leo'){
+                    intimacy += 2000; //基本盤, 懂?
+                }
+
                 if(intimacy > 9500){ //Means you are really lucky;
                     intimacy = ' Overflow! 已達上限 ! 大家快逃啊';
                 }
