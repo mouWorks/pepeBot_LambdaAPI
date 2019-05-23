adad12ed8f91ff32f69a27a1410df2c6db3f92c5
Leo.Kuo <et282523@hotmail.com>
2019-05-23 16:05:33 +0800

    拆測試

--- test/lib.js
+++ test/lib.js
@@ -2,7 +2,7 @@ const should = require('should');
 const lib = require('../libs/lib');
 const miment = require('miment');
 
-describe('#libs/rand', () => {
+describe('#libs/lib.getCountDownDate', () => {
     // 倒數計時
     it('Mou 倒數測試 2019-04-02 應為 2', done => {
         var today = "2019-04-02";
@@ -17,7 +17,9 @@ describe('#libs/rand', () => {
         v.should.greaterThan(1);
         done();
     });
+});
 
+describe('#libs/lib.recognizePeople', () => {
     it('取得 speaker', done => {
         let v = lib.recognizePeople("U2cdda2ee76097f9021bb68e70feffdb4");
         v.should.eqls('leo');
--- test/rand.js
+++ test/rand.js
@@ -1,19 +1,12 @@
 const should = require('should');
 const rand = require('../libs/rand');
 
-describe('#libs/rand', () => {
+describe('#libs/rand.getFromArray', () => {
     // 從陣列隨機抽出 1 個值
     it('從陣列隨機抽出 1 個值', done => {
         const v = rand.getFromArray([1, 2, 3, 4]);
         // console.log(v);
         v.should.Number();
-        done();
-    });
-
-    it('隨機出 1 個不超過最大值的值', done => {
-        const v = rand.getValue(999);
-        // console.log(v);
-        v.should.Array();
         done();
     });
 
@@ -24,6 +17,15 @@ describe('#libs/rand', () => {
         v.should.eqls(5);
         done();
     });
+});
+
+describe('#libs/rand.getValue', () => {
+    it('隨機出 1 個不超過最大值的值', done => {
+        const v = rand.getValue(999);
+        // console.log(v);
+        v.should.Array();
+        done();
+    });
 
     it('取得上下限中隨機的一個數字', done => {
         const v = rand.getNumber(1, 1);
@@ -32,3 +34,13 @@ describe('#libs/rand', () => {
         done();
     });
 });
+
+describe('#libs/rand.getNumber', () => {
+    it('取得上下限中隨機的一個數字', done => {
+        const v = rand.getNumber(1, 1);
+        v.should.Number();
+        v.should.eqls(1);
+        done();
+    });
+});
+
