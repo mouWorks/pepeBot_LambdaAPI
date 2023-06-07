9f8eee11915da220f5178d8a2b19959dade8067e
Leo Kuo <et282523@hotmail.com>
2023-06-07 11:36:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -364,7 +364,7 @@ exports.handler = function (req, res) {
             
             case 'Q':
                 countdown = lib.getTimeRemaining(qnapStartTime);
-                var note = rand.getFromArray(['凡事太盡 緣份早盡', '去找吧，我把客訴都留在系統裡了', `qNapper Rises`, `去怪你的網路公司吧`])
+                var note = rand.getFromArray(['凡事太盡 緣份早盡', '去找吧，我把客訴都留在系統裡了', `去怪你的網路公司吧`])
                 var timeRangeDesc = `剩下 ${countdown.days} 天 ${countdown.hours} 小時 ${countdown.minutes} 分 ${countdown.seconds} 秒`
                 var text = 'Qnap 客訴工程師生涯倒數' + timeRangeDesc
                 messages[0] = messageBuilder.flexCountDown(text, '緣盡倒數', timeRangeDesc, note);
